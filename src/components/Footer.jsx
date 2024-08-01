1ab52d66f3bb2f846ce18ab121c8a576c56fedd3
Photic23 <resellcourtesyx@gmail.com>
2024-08-01 21:22:41 +0700

    minor size fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <div className="fixed  w-full px-8 py-8 bottom-0" >
         <div class=" w-full inline-flex justify-center rounded border-2 border-black px-1 py-1 bg-[#A1CDA8]">
             <div class="flex justify-center align-center items-center buttonName h-9">
-                <p className='font-bold'>Copyright &copy; 2024 Photic23. All Rights Reserved</p>
+                <p className='text-sm sm:text-base text-center font-bold'>Copyright &copy; 2024 Photic23. All Rights Reserved</p>
             </div>
             <div class="items-center inline-flex">
 
