4ebdf153ae16f87351e6759700f18a55a1c401fe
Photic23 <resellcourtesyx@gmail.com>
2025-04-18 13:52:58 +0700

    fix type

--- src/components/BlogButton.jsx
+++ src/components/BlogButton.jsx
@@ -6,9 +6,10 @@ function BlogButton() {
 
 
     return(
-        <div className="w-11/12 bg-[#e6f9fc] border-2 border-black h-12 rounded-lg">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores molestias quam enim ducimus voluptate ex blanditiis minus rerum molestiae quis, totam repellendus sapiente quas fugit beatae cum repudiandae perspiciatis porro.</p>
-        </div>
+        <></>
+        // <div className="w-11/12 bg-[#e6f9fc] border-2 border-black h-12 rounded-lg">
+        //     {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores molestias quam enim ducimus voluptate ex blanditiis minus rerum molestiae quis, totam repellendus sapiente quas fugit beatae cum repudiandae perspiciatis porro.</p> */}
+        // </div>
     );
 }
 
